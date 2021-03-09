651d2d530006964b41eec3f97ee406ed4a0087a2
Jessica Embury <juannajane@Jessicas-MacBook-Air.local>
2021-03-09 09:23:47 -0800

    insert logo to nav

--- client/src/components/Shared/Nav/Nav.jsx
+++ client/src/components/Shared/Nav/Nav.jsx
@@ -1,3 +1,18 @@
 import './Nav.css'
 import { NavLink } from 'react-router-dom'
 
+
+const Nav = ({ user }) => {
+  return (
+    <nav>
+      <div className='nav'>
+        <NavLink className="logo" to="/"><img src='http://westtownbikes.org/wp-content/uploads/2018/08/cropped-WTB_Logo-1.jpg' alt='logo' /></NavLink>
+        {/* <div className="links">
+                        {user && <div className="link welcome">Welcome, {user.username}</div>}
+                        {alwaysOptions}
+                        {user ? authenticatedOptions : unauthenticatedOptions}
+                    </div> */}
+      </div>
+    </nav>
+  )
+}
