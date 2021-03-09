a06ccd0ff0ad29d013051d7fdf174b0e9e5a89b8
Wifey <wifey@Adrians-MacBook-Air.local>
2021-03-09 18:15:23 +0000

    updated Home.jsx to test rendering, corrected import path

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Home.css'
-import Layout from '../../components/shared/Layout/Layout'
+import Layout from '../../components/Shared/Layout/Layout'
 
 const Home = (props) => {
   return (
-    <Layout user={props.user}>
+    
       <div className="home">
+        Test test
       </div>
-    </Layout>
+    
   )
 }
 
