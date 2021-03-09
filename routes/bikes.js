b7a498ad12a4d98db3b7fe92f8c5a581d56c0120
Pat Ray <patraydev@gmail.com>
2021-03-09 13:12:06 -0600

    FULL CRUD AND TESTING ACHIEVED

--- routes/bikes.js
+++ routes/bikes.js
@@ -4,7 +4,7 @@ const controllers = require('../controllers/bikes');
 const router = Router();
 
 router.get('/bikes', controllers.getBikes);
-router.get('/bikes/:id', controllers.getBikes);
+router.get('/bikes/:id', controllers.getBike);
 router.post('/bikes', controllers.createBike);
 router.put('/bikes/:id', controllers.updateBike);
 router.delete('/bikes/:id', controllers.deleteBike);
