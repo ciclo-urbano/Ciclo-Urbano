dff4d95e84718627dfe68e38769a944609f43654
Jessica Embury <juannajane@Jessicas-MacBook-Air.local>
2021-03-09 09:29:13 -0800

    import nav to layout

--- client/src/components/Shared/Nav/Nav.jsx
+++ client/src/components/Shared/Nav/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = ({ user }) => {
   return (
     <nav>
       <div className='nav'>
-        <NavLink className="logo" to="/"><img src='http://westtownbikes.org/wp-content/uploads/2018/08/cropped-WTB_Logo-1.jpg' alt='logo' /></NavLink>
+        <NavLink className="logo" to="/"><img src='http://westtownbikes.org/wp-content/uploads/2018/08/cropped-WTB_Logo-1.jpg' alt='logo'/></NavLink>
         {/* <div className="links">
                         {user && <div className="link welcome">Welcome, {user.username}</div>}
                         {alwaysOptions}
