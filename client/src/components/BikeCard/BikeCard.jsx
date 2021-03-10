6a13017641ce67282584af6402ada11df374be98
Wifey <wifey@Adrians-MacBook-Air.local>
2021-03-10 20:53:26 +0000

    correct image_url to imageURL, bikes rendering on carousel

--- client/src/components/BikeCard/BikeCard.jsx
+++ client/src/components/BikeCard/BikeCard.jsx
@@ -5,7 +5,7 @@ function BikeCard(props) {
   return (
     <div className='bike-card'>
       <Link className='card' to={`/bikes/${props.id}`}>
-        <img className='bike-card-image' src={props.image_url} alt={props.title}/>
+        <img className='bike-card-image' src={props.imageURL} alt={props.title}/>
         <p className="card-model">{props.model}</p>
       </Link>
     </div>
