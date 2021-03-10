aeeb6f5316cfc20f18f5681a36ef7a332df3de47
Pat Ray <patraydev@gmail.com>
2021-03-10 11:42:37 -0600

    fixed to auth controllers

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const express = require('express');
 const cors = require('cors')
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-const routes = require('./routes');
-
+const bikeRoutes = require('./routes/bikes');
+const userRoutes = require('./routes/users');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-app.use('/api', routes);
+app.use('/api', bikeRoutes);
+app.use('/api', userRoutes);
 
 module.exports = app;
