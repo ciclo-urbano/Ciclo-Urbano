063ff64fd682709b985f12a057d31e7afc99154c
Pat Ray <patraydev@gmail.com>
2021-03-11 09:25:03 -0600

    commented out useHistory in BikeAdd

--- client/src/screens/BikeAdd/BikeAdd.jsx
+++ client/src/screens/BikeAdd/BikeAdd.jsx
@@ -1,6 +1,6 @@
 import Form from '../../components/Form/Form.jsx';
 import Layout from '../../components/Shared/Layout/Layout';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 import './BikeAdd.css'
 
 function BikeAdd(props) {
