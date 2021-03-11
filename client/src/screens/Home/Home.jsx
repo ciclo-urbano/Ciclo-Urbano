98ed9c2be64463395854f7208db629439eee2f32
Wifey <wifey@Adrians-MacBook-Air.local>
2021-03-11 19:51:08 +0000

    - added welcome header/image to Home screen - adjusted responsiveness of Slider images

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -6,7 +6,16 @@ import Layout from '../../components/Shared/Layout/Layout'
 const Home = (props) => {
   return (
     <Layout user={props.user}>
-      <p>Featured Bikes</p>
+     <div className="welcome-container">
+      <div className="welcome-picture">
+        <img src="https://images.unsplash.com/photo-1596805827513-33a7b2523abf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80" />
+      </div>
+      <div className="welcome-header">
+          <p>BIKES MADE WITH 💕</p>
+          <span className="community">For our community, by the community🚲</span>
+        </div>
+      </div>  
+      <p className="featured-header">Featured Bikes</p>
       <SimpleSlider />
     </Layout>
   )
