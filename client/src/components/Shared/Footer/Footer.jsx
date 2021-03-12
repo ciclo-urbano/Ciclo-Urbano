685340430262ed9f964bf66e2506961987aebe59
Wifey <wifey@Adrians-MacBook-Air.local>
2021-03-12 16:16:09 +0000

    updated Footer with our Github links

--- client/src/components/Shared/Footer/Footer.jsx
+++ client/src/components/Shared/Footer/Footer.jsx
@@ -1,10 +1,27 @@
 import { Link } from "react-router-dom";
-import './Footer.css'
+import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-      <p>Project by Jessica Embury, Nathan Wood, Pat Ray, & Daphne Melvin</p>
+      <p>
+        Project by{" "}
+        <a href="https://www.github.com/dollydoxyfree" target="_blank">
+          Jessica Embury
+        </a>
+        ,{" "}
+        <a href="https://www.github.com/nathanwould" target="_blank">
+          Nathan Wood
+        </a>
+        ,{" "}
+        <a href="https://www.github.com/patraydev" target="_blank">
+          Pat Ray
+        </a>
+        , &{" "}
+        <a href="https://www.github.com/dmelvy" target="_blank">
+          Daphne Melvin
+        </a>
+      </p>
     </div>
   );
 }
