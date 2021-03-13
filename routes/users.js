71a7bd6699ed05201af66ca3e466eac3a85b70f6
Pat Ray <patraydev@gmail.com>
2021-03-13 12:51:21 -0600

    added assoc routes

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,18 @@
-const { Router } = require('express')
-const controllers = require('../controllers/users')
+const { Router } = require('express');
+const controllers = require('../controllers/users');
 
-const router = Router()
+const router = Router();
 
-router.post('/sign-up', controllers.signUp)
-router.post('/sign-in', controllers.signIn)
-router.get('/verify', controllers.verify)
+//auth routes
+router.post('/sign-up', controllers.signUp);
+router.post('/sign-in', controllers.signIn);
+router.get('/verify', controllers.verify);
+
+//assoc routes
+router.get('/users', controllers.getUsers);
+router.get('./users/:id', controllers.getBikesByUser);
+
+//ambitious?
 // router.post('/change-password', controllers.changePassword)
 
 module.exports = router
