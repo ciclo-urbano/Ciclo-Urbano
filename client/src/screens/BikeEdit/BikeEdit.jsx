4e9b47f3af7b95491e116914ecfbf92c0a2a50a2
Pat Ray <patraydev@gmail.com>
2021-03-15 18:46:34 -0500

    put image into bike edit component

--- client/src/screens/BikeEdit/BikeEdit.jsx
+++ client/src/screens/BikeEdit/BikeEdit.jsx
@@ -1,14 +1,34 @@
 import Form from "../../components/Form/Form.jsx";
 import Layout from "../../components/Shared/Layout/Layout";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getBike } from "../../services/bikes";
 import "./BikeEdit.css";
 
 function BikeEdit(props) {
   const { id } = useParams();
+  const [bike, setBike] = useState(null);
+
+  useEffect(() => {
+    const fetchBike = async () => {
+      const theBike = await getBike(id);
+      setBike(theBike);
+    };
+    fetchBike();
+
+    // Image upload could go here if/else
+  }, [id]);
 
   return (
     <Layout user={props.user}>
       <div className="form-background">
+        <div className="image-box">
+        {bike ? (
+          <img src={bike.imageURL} alt="bike" className="edit-image" />
+        ) : (
+          <h1>Loading...</h1>
+          )}
+          </div>
         <Form id={id} />
       </div>
     </Layout>
