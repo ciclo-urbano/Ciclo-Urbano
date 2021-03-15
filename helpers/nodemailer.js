08275c2faf8f084696a6c1053df10d477162412e
Pat Ray <patraydev@gmail.com>
2021-03-15 11:10:35 -0500

    added nodemailer helper

---
+++ helpers/nodemailer.js
@@ -0,0 +1,20 @@
+const nodemailer = require("nodemailer");
+require('dotenv').config();
+
+const sendEmail = async (email) =>{
+
+//use test acct to send email
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail(email);
+
+}
+
+module.exports = sendEmail;
