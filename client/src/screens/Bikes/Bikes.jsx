7b0e8205f5d1b9ac8049cbd07f57173a763fd4d6
Pat Ray <patraydev@gmail.com>
2021-03-16 08:58:27 -0500

    removed Link import from Bikes screen

--- client/src/screens/Bikes/Bikes.jsx
+++ client/src/screens/Bikes/Bikes.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './Bikes.css';
 import Bike from '../../components/Bike/Bike';
 import Search from '../../components/Search/Search';
