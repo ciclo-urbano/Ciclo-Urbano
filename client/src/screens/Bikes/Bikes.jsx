4003a1e00c342fbc420e1a43601fe4632cb4801e
Wifey <wifey@Adrians-MacBook-Air.local>
2021-03-16 14:43:46 +0000

    removed unused Links import to remove console warning

--- client/src/screens/Bikes/Bikes.jsx
+++ client/src/screens/Bikes/Bikes.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import './Bikes.css';
 import Bike from '../../components/Bike/Bike';
 import Search from '../../components/Search/Search';
